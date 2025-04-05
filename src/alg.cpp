771b1e039a2b24b5259381a7eaf3df5627bd5026
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-04-05 23:50:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,8 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int counter = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = i+1; j < len; j++) {
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         counter++;
       }
@@ -17,21 +17,35 @@ int countPairs2(int *arr, int len, int value) {
   int countl;
   int left = 0;
   int right = len - 1;
+
   while (left < right) {
     if (arr[left] + arr[right] == value) {
       countl = 1;
       countr = 1;
-      while (arr[left] == arr[left + 1] && left + 1 < right) {
+
+      int left_orig = left;
+      int right_orig = right;
+
+      while (left + 1 < right && arr[left] == arr[left + 1]) {
         countl++;
         left++;
       }
-      while (arr[right] == arr[right - 1] && right - 1 > left) {
+      while (right - 1 > left && arr[right] == arr[right - 1]) {
         countr++;
         right--;
       }
-      counter += countl * countr;
-      left++;
-      right--;
+      if (left == right) {
+          counter += countl * countr;
+      }
+      else{
+        counter += countl * countr;
+      }
+
+      left = left_orig + countl;
+      right = right_orig - countr;
+      if (left >= right_orig){
+          break;
+      }
     } else if (arr[left] + arr[right] > value) {
       right--;
     } else {
@@ -42,40 +56,42 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 int searchNum(int *arr, int len, int num) {
-  bool isCycle = false;
   int counter = 0;
   int left = 0;
   int right = len - 1;
-  int center = left + (right - left) / 2;
+  int center;
+
   while (left <= right) {
+    center = left + (right - left) / 2;
+
     if (arr[center] == num) {
-      while (arr[center + 1] == num && center + 1 < len) {
-        center++;
-      }
-      while (arr[center - 1] == num && center - 1 >= 0) {
+      counter = 1;
+      int temp_center = center;
+
+      while (temp_center + 1 < len && arr[temp_center + 1] == num) {
         counter++;
-        center--;
-        isCycle = true;
+        temp_center++;
       }
-      if (!isCycle) {
+      temp_center = center;
+      while (temp_center - 1 >= 0 && arr[temp_center - 1] == num) {
         counter++;
+        temp_center--;
       }
-      break;
+      return counter;
     } else if (arr[center] > num) {
       right = center - 1;
-      center = left + (right - left) / 2;
     } else {
       left = center + 1;
-      center = left + (right - left) / 2;
     }
   }
-  return counter;
+  return 0;
 }
 
 int countPairs3(int *arr, int len, int value) {
   int counter = 0;
-  for (int i = 0; i < len - 1; i++) {
-    counter += searchNum(arr + i + 1, len - i + 1, value - arr[i]);
+  for (int i = 0; i < len; i++) {
+    counter += searchNum(arr + i + 1, len - i - 1, value - arr[i]);
   }
   return counter;
 }
+
