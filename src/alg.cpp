7469dfb5ee170ba3966cd188c95a94a57ddfbc7e
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-04-05 19:43:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,14 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int counter = 0;
+  for (i = 0; i < len; i++) {
+    for (j = 0; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        counter++;
+      }
+    }
+  }
+  return counter;
 }
 int countPairs2(int *arr, int len, int value) {
   return 0;
